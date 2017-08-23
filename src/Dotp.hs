88ae72a2cba59dc1289b8dde72f65d250115c6f0
= <pwoh@live.com>
2017-08-24 00:56:22 +1000

    Some more notes - matmul doesn't work, needs to be a matrix. No buitl in fold or zip functions in tf-haskell? Data.Vector.Storable works but Data.Storable idk how to use.

--- src/Dotp.hs
+++ src/Dotp.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE FlexibleContexts #-}
 
 import Control.Monad (replicateM, replicateM_, zipWithM, foldM)
@@ -14,7 +13,8 @@ import qualified TensorFlow.Tensor as TF
 import qualified TensorFlow.Types as TF
 import qualified TensorFlow.Variable as TF hiding (variable, assign)
 import Data.Int (Int32, Int64)
-import qualified Data.Vector as V
+import qualified Data.Vector.Storable as V
+--import qualified Data.Storable as S -- how to use this one?
 import Test.HUnit ((@=?))
 
 
@@ -26,7 +26,7 @@ import Prelude
 
 import qualified Data.Text as Text
 --dotp :: Acc (Vector Double) -> Acc (Vector Double) -> Acc (Scalar Double)
-dotp xs ys = foldl (+) 0 (zipWithM (*) xs ys)
+--dotp xs ys = foldl (+) 0 (zipWithM (*) xs ys)
 
 
 --dotp_unvectorised = lambda xs, ys: tf.foldl(lambda a, x: a + x, tf.map_fn(lambda x: x[0] * x[1], (xs,ys), dtype=tf.float32))
@@ -35,47 +35,53 @@ dotp xs ys = foldl (+) 0 (zipWithM (*) xs ys)
 
 main :: IO ()
 main = do
-    --(f,a,b) <- derp
-    --(z,zz) <- derp :: IO ((Float, Float))
     z <- derp 
-    x <- (eval $ TF.reducedShape (TF.vector [2, 3, 5, 7 :: Int64])
-                                (TF.vector [1, 2 :: Int32])) :: IO (V.Vector Int32)
-    --V.fromList [2, 1, 1, 7 :: Int32] @=? x -- this fixes the ambiguous shape?
-
-
-    --putStrLn $ show z
-    --putStrLn $ show zz
-    --putStrLn $ show x
-    --putStrLn $ show f
-    --putStrLn $ show a
-    --putStrLn $ show b
-    -- Generate data where `y = x*3 + 8`.
-   -- xData <- replicateM 100 randomIO
-    --let yData = [x*3 + 8 | x <- xData]
-    --TF.runSession $ TF.run $ TF.print a
+    putStrLn $ show z
     return ()
 
-eval :: TF.Fetchable t a => t -> IO a
-eval = TF.runSession . TF.run
+-- Doesn't work - not a matrix?
+--derp2 :: IO (Float)
+--derp2 = TF.runSession $ do
+--    let x = TF.vector [1.0, 2.0, 3.0 :: Float]
+--        y = TF.vector [1.0, 2.0, 3.0 :: Float] 
+--    vx <- TF.variable (TF.Shape [3])
+--    wx <- TF.assign vx x
+--    vy <- TF.variable (TF.Shape [3])
+--    wy <- TF.assign vy x
+--    vz <- TF.variable (TF.Shape [3])
+--    wz <- TF.assign vz (wx `TF.matMul` wy)
+--    TF.Scalar result <- TF.run (wz)
+--    return result
 
-
--- derp :: IO (Float, V.Vector Float, Float)
+derp :: IO (V.Vector Float, Float)
 derp = TF.runSession $ do 
     let x = TF.vector [1.0, 2.0, 3.0 :: Float]
         y = TF.vector [1.0, 2.0, 3.0 :: Float]
-    v <- TF.variable (TF.Shape [3])
-    w <- TF.assign v x
+    vx <- TF.variable (TF.Shape [3])
+    wx <- TF.assign vx x
+    vy <- TF.variable (TF.Shape [3])
+    wy <- TF.assign vy x
+    vz <- TF.variable (TF.Shape [3])
+    wz <- TF.assign vz (wx `TF.mul` wy)
+    result <- TF.run wz
+
+    TF.Scalar result2 <- TF.run (TF.reduceSum wy)
+
+    return (result, result2)
+
     --TF.Scalar z <- TF.run $ TF.reduceSum w
 -- Without TF.Scalar, I get 'No instance for (TF.Fetchable (TF.Tensor TF.Build Float) Float)
       --  arising from a use of ‘TF.run’'
 
     --TF.Scalar z <- TF.run $ TF.readValue v
     --return z
-    TF.Scalar z <- TF.run $ TF.reduceSum w
-    w' <- TF.run w :: TF.Session (V.Vector Float) -- XXX needs type signature on TF.run
-    s  <- TF.run w -- or use the result as a Vector somewhere, or top-level type signature, etc...
-    -- Data.Storable check if that works.
-    return (z, w', V.sum s)
+
+    --TF.Scalar z <- TF.run $ TF.reduceSum w
+    --w' <- TF.run w :: TF.Session (V.Vector Float) -- XXX needs type signature on TF.run
+    --s  <- TF.run w -- or use the result as a Vector somewhere, or top-level type signature, etc...
+    ---- Data.Storable check if that works.
+    --return (z, w', V.sum s)
+
     --(TF.Scalar z, TF.Scalar zz) <- TF.run (TF.reduceSum x, TF.reduceSum w)
     --return (z, zz)
 
