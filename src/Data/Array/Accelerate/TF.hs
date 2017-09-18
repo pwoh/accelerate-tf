e09facc39e981cd1b9495f317af2f6b220bd8a82
= <pwoh@live.com>
2017-09-18 17:56:11 +1000

    Shapes and basic plusone example

--- src/Data/Array/Accelerate/TF.hs
+++ src/Data/Array/Accelerate/TF.hs
@@ -72,30 +72,52 @@ evalOpenAcc
 1) Print the aenv possibly to look at it. -- not sure how to do this.
 2) Print function properly with PrimApp and PrimAdd. [done]
 2.5) implement variables properly for env. should i have my own env? probably not - use AST?
+2.6) get shape working - convert to tf shape [done]
 3) Test PlusOne, and print out what my tensorflow should do.
 4) Pattern match Let. Write simple example with Let. Use environments.
 5) Implement Zipwith.
 6) Test Dotp.
 7) Make it actually run instead of print? keep print version for debugging.
 -}
-    
-evalOpenAcc (AST.OpenAcc (AST.Use a')) aenv' = "[Using" P.++ myShowArrays (toArr a' :: a) P.++ "]"
-evalOpenAcc (AST.OpenAcc (AST.Map f (a'))) aenv' = "[Fun: " P.++ (evalLam f Empty aenv') P.++ " => " P.++ evalOpenAcc a' aenv' P.++ "]"
+
+evalOpenAcc (AST.OpenAcc (AST.Use a')) aenv' = "[Use TF.constant" P.++ myArrayShapes (toArr a' :: a) P.++ myShowArrays (toArr a' :: a) P.++ "]"
+evalOpenAcc (AST.OpenAcc (AST.Map f (a'))) aenv' = "[Fun: " P.++ (evalLam f (Empty `Push` arrVal) aenv') --P.++ " => " P.++ evalOpenAcc a' aenv' P.++ "]"
+    where arrVal = evalOpenAcc a' aenv'
 evalOpenAcc _ _ = "???"
 
 evalLam :: AST.PreOpenFun f env aenv t -> TFEnv env2 -> AST.Val aenv -> String
-evalLam (AST.Lam f) env' aenv' = evalLam f (env' `Push` "meep") aenv' --assume single var for now?
-evalLam (AST.Body (AST.PrimApp (AST.PrimAdd eltType) (AST.Tuple args))) env' aenv' = "Add " P.++ show eltType P.++ show (evalTuple args env' aenv')
+evalLam (AST.Lam f) env' aenv' = evalLam f (env') aenv' --assume single var for now?
+evalLam (AST.Body (AST.PrimApp (AST.PrimAdd eltType) (AST.Tuple args))) env' aenv' = "TF.add "{-P.++ show eltType-}  P.++ show (evalTuple args env' aenv')
 
 -- scalar expr
 evalPreOpenExp :: forall acc env aenv t env2. AST.PreOpenExp acc env aenv t -> TFEnv env2 -> AST.Val aenv -> String
 evalPreOpenExp (AST.Var ix) env' aenv = "var..." P.++ show (tfprj ix env')  -- first do constant or variable look up 
-evalPreOpenExp (AST.Const c) _ _ = "const..." P.++ show (Sugar.toElt c :: t)  -- first do constant or variable look up 
+evalPreOpenExp (AST.Const c) _ _ = "TF.constant (TF.Shape []) [" P.++ show (Sugar.toElt c :: t) P.++ "]" -- first do constant or variable look up 
 evalPreOpenExp _ _ _ = "..."  -- first do constant or variable look up 
 
 evalTuple :: Tuple (AST.PreOpenExp acc env aenv) t -> TFEnv env2 -> AST.Val aenv -> [String]
 evalTuple (Sugar.SnocTup xs x) env' aenv' = (evalPreOpenExp x env' aenv'):(evalTuple xs env' aenv')
 evalTuple (Sugar.NilTup) env' aenv' = []
+
+
+myArrayShapes :: forall arrs. Arrays arrs => arrs -> String
+myArrayShapes = display . collect (arrays (undefined::arrs)) . Sugar.fromArr
+  where
+    collect :: ArraysR a -> a -> [String]
+    collect ArraysRunit         _        = []
+    collect ArraysRarray        arr      = [myShowArrayShape arr]
+    collect (ArraysRpair r1 r2) (a1, a2) = collect r1 a1 P.++ collect r2 a2
+    --
+    display []  = []
+    display [x] = x
+    display xs  = "(" P.++ List.intercalate ", " xs P.++ ")"
+
+myShowArrayShape :: (Shape sh, Elt e) => Array sh e -> String
+myShowArrayShape arr = "{TF.Shape " P.++ myShowShape (Sugar.shape arr) P.++ "}"
+--  = "{" P.++ Sugar.showShape (Sugar.shape arr) P.++ "}"
+  
+myShowShape :: Shape sh => sh -> String
+myShowShape shape = show $ shapeToList shape
 
 myShowArrays :: forall arrs. Arrays arrs => arrs -> String
 myShowArrays = display . collect (arrays (undefined::arrs)) . Sugar.fromArr
